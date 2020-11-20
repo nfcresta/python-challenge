d7920efad4110780bfe8bcdea9e33c4df2068b42
nfcresta <72232054+nfcresta@users.noreply.github.com>
2020-11-20 15:11:21 -0500

    Final Changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,11 +15,7 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
 
-    # # print rows
-    # for row in csvreader:
-    #     print(row)
-
-
+    # Print first title of analysis
     print("Financial Analysis")
     print('-------------------------------')
 
@@ -29,46 +25,46 @@ with open(csvpath) as csvfile:
         # calculate total nuber of months included in the dataset
         totalMonths.append(row[0])
     
-        # calculate total Profit/Losses and find the average change
+        # calculate total Profit/Losses to find the average change
         totalPL.append(int(row[1]))
 
     # get monthly change in profits
-    for i in range(len(totalPL) - 1):
+    for c in range(len(totalPL) - 1):
         
         # find difference between months and append to the monthlyChange
-        monthlyChange.append(totalPL[i+1] - totalPL[i])
+        monthlyChange.append(totalPL[c+1] - totalPL[c])
 
 # get maximum and minimum change
 max_increase = max(monthlyChange)
 max_decrease = min(monthlyChange)
 
-# get maximum and minimum change
+# get maximum and minimum change (use plus one at the end of formula to get the actual month where change occurred)
 greatest_increase_M = monthlyChange.index(max(monthlyChange)) + 1
 greatest_decrease_M = monthlyChange.index(min(monthlyChange)) + 1
 
-# print results
+# print results in terminal
 print(f'Total Months: {len(totalMonths)}')
 print(f'Total: ${sum(totalPL)}')
 print(f'Average Change: ${round(sum(monthlyChange)/len(monthlyChange), 2)}')
 print(f'Greatest Increase in profits: {totalMonths[greatest_increase_M]} (${str(max_increase)})')
 print(f'Greatest Decrease in profits: {totalMonths[greatest_decrease_M]} (${(str(max_decrease))})')
 
+
 # Export a txt file with the results
-output_path = os.path.join("analysis", "Financial_Analysis.txt")
+output_path = os.path.join("analysis", "financial_analysis.txt")
 
 # open output file and write
 with open(output_path, "w") as file:
     file.write('Financial Analysis')
     file.write('\n')
-    file.write('-------------------------------')
+    file.write('--------------------------------')
     file.write('\n')
     file.write(f'Total Months: {len(totalMonths)}')
     file.write('\n')
     file.write(f'Total: ${sum(totalPL)}')
     file.write('\n')
-    file.write(f'Average Change: {round(sum(monthlyChange)/len(monthlyChange),2)}')
+    file.write(f'Average Change: ${round(sum(monthlyChange)/len(monthlyChange),2)}')
     file.write('\n')
     file.write(f'Greatest Increase in Profits: {totalMonths[greatest_increase_M]} (${(str(max_increase))})')
     file.write('\n')
     file.write(f'Greatest Decrease in Profits: {totalMonths[greatest_decrease_M]} (${(str(max_decrease))})')
-
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,56 +5,75 @@ import csv
 # Create path for csv
 csvpath = os.path.join("Resources", "election_data.csv")
 
+# Create lists and variables to use for analysis
 totalVotes = 0
-Khan_votes = 0
-Correy_votes = 0
-Li_votes = 0
-OTooley_votes = 0
+candidates = []
+votes = []
+percentVotes = []
 
 # open csv file
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
-    # print(f'{header}')
 
-    # loop through rows to find votes
+    # loop through rows
     for row in csvreader:
+        
         # Add to our vote-counter 
-        total_votes += 1 
-
-        '''
-        If the candidate is not on our list, add his/her name to our list, along with 
-        a vote in his/her name.
-        If he/she is already on our list, we will simply add a vote in his/her
-        name 
-        '''
+        totalVotes = totalVotes + 1 
+        
+        # Add unique candidates to the list and their respective votes
         if row[2] not in candidates:
             candidates.append(row[2])
-            index = candidates.index(row[2])
-            num_votes.append(1)
+            candidate_index = candidates.index(row[2])
+            votes.append(1)
+        
+        # For candidates already in the list, add the respective vote to their name
         else:
-            index = candidates.index(row[2])
-            num_votes[index] += 1
+            candidate_index = candidates.index(row[2])
+            votes[candidate_index] = votes[candidate_index] + 1
     
-    # Add to percent_votes list 
-    for votes in num_votes:
-        percentage = (votes/total_votes) * 100
-        percentage = round(percentage)
+    # Append the percentVotes list and initialize for printing later 
+    for vote in votes:
+        percentage = round((vote/totalVotes) * 100)
         percentage = "%.3f%%" % percentage
-        percent_votes.append(percentage)
+        percentVotes.append(percentage)
     
-    # Find the winning candidate
-    winner = max(num_votes)
-    index = num_votes.index(winner)
-    winning_candidate = candidates[index]
+    # Identify candidate with the most votes
+    # winner = max(votes)
+    winner_index = votes.index(max(votes))
+    winningCandidate = candidates[winner_index]
 
-# Displaying results
+# Print results in terminal
 print("Election Results")
-print("--------------------------")
-print(f"Total Votes: {str(total_votes)}")
-print("--------------------------")
-for i in range(len(candidates)):
-    print(f"{candidates[i]}: {str(percent_votes[i])} ({str(num_votes[i])})")
-print("--------------------------")
-print(f"Winner: {winning_candidate}")
-print("--------------------------")
+print("---------------------------")
+print(f"Total Votes: {str(totalVotes)}")
+print("---------------------------")
+for c in range(len(candidates)):
+    print(f"{candidates[c]}: {str(percentVotes[c])} ({str(votes[c])})")
+print("---------------------------")
+print(f"Winner: {winningCandidate}")
+print("---------------------------")
+
+
+# Export to txt file with the results
+output_path = os.path.join("analysis", "election_analysis.txt")
+
+# Open output file and write
+with open(output_path, "w") as file:
+    file.write('Election Results')
+    file.write('\n')
+    file.write('---------------------------')
+    file.write('\n')
+    file.write(f'Total Votes: {str(totalVotes)}')
+    file.write('\n')
+    file.write('---------------------------')
+    file.write('\n')
+    for c in range(len(candidates)):
+        file.write(f'{candidates[c]}: {str(percentVotes[c])} ({str(votes[c])})')
+        file.write('\n')
+    file.write('---------------------------')
+    file.write('\n')
+    file.write(f'Winner: {winningCandidate}')
+    file.write('\n')
+    file.write('---------------------------')
